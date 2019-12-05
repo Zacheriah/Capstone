46f55ca06c64d82c778a2618b44d359867a46aa6
Wayman <zwayman@cci14sj1gqaws.dyn.uncc.edu>
2019-12-05 16:36:58 -0500

    Hardcoded T rex database reference working now. Just need to update inventory on profile screen as well as join date and dinos found

--- Test/DigViewController.swift
+++ Test/DigViewController.swift
@@ -7,21 +7,35 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
 
 class DigViewController: UIViewController {
-
+    var userid = Auth.auth().currentUser?.uid
+    var dbRef = Database.database().reference()
     @IBOutlet weak var topLayer: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.bringSubviewToFront(topLayer)
         topLayer.isUserInteractionEnabled = true
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(getter: topLayer))
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         topLayer.addGestureRecognizer(tapGesture)
-        
+
         // Do any additional setup after loading the view.
     }
-    
+    @objc func handleTap(sender: UITapGestureRecognizer){
+        if sender.state == .ended{
+            self.topLayer.image = nil
+            dbRef.child(userid!).child("dinos").childByAutoId().updateChildValues(["name" : "T-rex", "diet": "Carnivore", "Period" : "Cretaceous" ]){ (error, dbRef) in
+                if error == nil{
+                    self.dismiss(animated: true, completion: nil)
+                }else{
+                    print(error!.localizedDescription)
+                }
+        }
+    }
+    }
 
     /*
     // MARK: - Navigation
